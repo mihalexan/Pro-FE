80d284019d9bf24553ac0a9dd711cf4ac9b89c01
Alexandra <al1kosareva.sasha@gmail.com>
2023-10-28 22:42:22 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleFullList() {
+  var fullList = document.getElementById("fullList");
+  if (fullList.style.display === "none") {
+    fullList.style.display = "block"; // Показать элемент fullList
+  } else {
+    fullList.style.display = "none"; // Скрыть элемент fullList
+  }
+}
